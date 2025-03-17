b88c48ede2373d2decde3102bd7d8cee57370c0b
Shehapsamer <https://github.com/Shehapsamer>
2025-03-18 00:24:56 +0200

    Updated frontend with new CORS settings and Stripe URLs and vircelJson

--- server.js
+++ server.js
@@ -8,8 +8,6 @@ const { collection, addDoc, doc, setDoc, getDoc, writeBatch, increment } = requi
 const app = express();
 
 // إعداد CORS
-const cors = require("cors");
-
 app.use(cors({
   origin: ["http://localhost:5184", "https://big-store-bj54000.vercel.app"],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
